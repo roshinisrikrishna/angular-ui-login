500d1920cd8d87679c82a580da972d42a17b148b
roshinisrikrishna <roshinisrikrishna@gmail.com>
2018-09-03 14:17:11 +0530

    login commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sample-login';
+ uname:string;
+ upassword:string;
+ message:string;
+ messageStyle :string;
+ showValue:boolean;
+ registermessage:string;
+ constructor(){
+   this.uname="";
+   this.upassword="";
+ }
+
+ show(parameter)
+ {
+   if(parameter==='login')
+   {
+     this.showValue=false;
+    
+   }
+   else
+   {
+     this.showValue=true;
+   }
+   return this.showValue;
+ }
+ submit(){
+if(this.uname!=="")
+{
+  this.message=`Successfully logged in by ${this.uname}`;
+  this.messageStyle = 'greenText';
+
+}
+else{
+  this.message=`Could'nt log in `;
+  this.messageStyle = 'redText';
+
+
+}
+ }
+ register(){
+  if(this.uname!==""&&this.upassword!=="")
+  {
+    this.registermessage=`Successfully registered by ${this.uname}`;
+    this.messageStyle = 'greenText';
+  
+  }
+  else{
+    this.registermessage=`Could'nt register`;
+    this.messageStyle = 'redText';
+  
+  
+  }
+ }
 }
